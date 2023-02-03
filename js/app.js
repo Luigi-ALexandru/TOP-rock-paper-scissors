70e6df86b325b1ca3c380d3d393e42767aa9dfc5
Luigi-ALexandru <123297588+Luigi-ALexandru@users.noreply.github.com>
2023-02-03 19:05:50 +0000

    project updated

--- js/app.js
+++ js/app.js
@@ -1,49 +1,72 @@
-//This is a function within a function to avoid starting the function when you open the website and start it when you click the button instead
+let playerScore = 0;
+let computerScore = 0;  
+let numberOfRounds = 0;
+       
+function playRound(){
+    //computer input
+        function getComputerChoice() {
+            const array = ["Rock", "Paper", "Scissors"];
+            const randomIndex = array[Math.floor(Math.random() * 3)];
+            const item = randomIndex;
+            return item;            
+            }
+    let computerChoice = getComputerChoice();
+    let HumanWinner;
+    let ComputerWinner;
+    //player input
+    const playerChoice = prompt("Enter your selection: Rock, Paper or Scissors:");
 
-function log() {
-   
-        function getComputerChoice(x) {
-        //get random index value
-        const randomIndex = Math.floor(Math.random() * 3);
-        //get random item
-        const item = x[randomIndex];
-
-        return item;
-
-            
+    if (playerChoice === "Rock" && computerChoice === "Rock") {
+        alert("You're even for now!");
+    } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
+            HumanWinner = true;
+        alert("PlayerWins this round!");
+    } else if (playerChoice === "Rock" && computerChoice === "Paper") {
+            ComputerWinner = true;
+        alert("ComputerWins this round!");
+    } else if (playerChoice === "Scissors" && computerChoice === "Rock") {
+            ComputerWinner = true;
+        alert("ComputerWins this round!");
+    } else if (playerChoice === "Scissors" && computerChoice === "Scissors") {
+        alert("You're even for now!");
+    } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+            HumanWinner = true;
+        alert("PlayerWins this round!");
+    } else if (playerChoice === "Paper" && computerChoice === "Rock") {
+            HumanWinner = true;
+        alert("PlayerWins this round!");
+    } else if (playerChoice === "Paper" && computerChoice === "Paper") {
+        alert("You're even for now!");
+    } else if (playerChoice === "Paper" && computerChoice === "Scissors") {
+            ComputerWinner = true;
+        alert("ComputerWins this round!");
+    } else {
+        alert("Input not recognized. Make sure you enter 'Rock', 'Paper' or 'Scissors'. Try again!");
+    }
+        //  update the scores
+        if(HumanWinner === true) {
+            playerScore = playerScore + 1;
+            document.getElementById("sH").innerHTML = playerScore;
+        } else if (ComputerWinner === true) {
+            computerScore = computerScore + 1;
+            document.getElementById("sC").innerHTML = computerScore;
         }
 
-        const array = ["Rock", "Paper", "Scissors"];
-        const computerChoice = getComputerChoice(array);
-        console.log(computerChoice);
-
-        const playerChoice = prompt("Enter your selection: Rock, Paper or Scissors:");
-
-        if (playerChoice === "Rock" && computerChoice === "Rock") {
-            alert("You're even!");
-        } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
-            alert("PlayerWins!");
-        } else if (playerChoice === "Rock" && computerChoice === "Paper") {
-            alert("ComputerWins!");
-        } else if (playerChoice === "Scissors" && computerChoice === "Rock") {
-            alert("ComputerWins!");
-        } else if (playerChoice === "Scissors" && computerChoice === "Scissors") {
-            alert("You're even!");
-        } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
-            alert("PlayerWins!");
-        } else if (playerChoice === "Paper" && computerChoice === "Rock") {
-            alert("PlayerWins!");
-        } else if (playerChoice === "Paper" && computerChoice === "Paper") {
-            alert("You're even!");
-        } else if (playerChoice === "Paper" && computerChoice === "Scissors") {
-            alert("ComputerWins!");
-        } else {
-            alert("Not recognized. Make sure you enter 'Rock', 'Paper' or 'Scissors'. Try again!");
+        if(playerScore === 4) {
+            alert("You've won the game!");
+        } else if (computerScore === 4) {
+            alert("Computer won the game!");
+        } else if(numberOfRounds === 4 && playerScore !== 4 && computerScore !== 4) {
+            alert("Nobody wins the game...");
         }
+    
+      }
+      
+function game() {
+    for (i = 0; i <= 4; i++) {
+        playRound();
+            numberOfRounds = numberOfRounds + 1;
+        }
+    }
 
-}
-
-// The score system. How to implement:
-// I need to create an array to store the score
-// The score needs to increase in value so maybe an array is not the answer?
-// Maybe the score should be stored in a function that adds +1 everytime a player wins
+document.getElementById("myBtn").addEventListener("click", game);
