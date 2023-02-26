3e8b40f47e3c522defc774ac770ffe8fc503fb3e
Luigi-ALexandru <123297588+Luigi-ALexandru@users.noreply.github.com>
2023-02-26 21:27:54 +0000

    everything

--- js/app.js
+++ js/app.js
@@ -1,72 +1,100 @@
-let playerScore = 0;
-let computerScore = 0;  
-let numberOfRounds = 0;
-       
-function playRound(){
-    //computer input
-        function getComputerChoice() {
-            const array = ["Rock", "Paper", "Scissors"];
-            const randomIndex = array[Math.floor(Math.random() * 3)];
-            const item = randomIndex;
-            return item;            
-            }
-    let computerChoice = getComputerChoice();
-    let HumanWinner;
-    let ComputerWinner;
-    //player input
-    const playerChoice = prompt("Enter your selection: Rock, Paper or Scissors:");
+function getComputerChoice() {
+    array = ["rock", "paper", "scissors"];
+    let randomNum = array[Math.floor(Math.random() * array.length)];
+    return randomNum;
+};
 
-    if (playerChoice === "Rock" && computerChoice === "Rock") {
-        alert("You're even for now!");
-    } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
-            HumanWinner = true;
-        alert("PlayerWins this round!");
-    } else if (playerChoice === "Rock" && computerChoice === "Paper") {
-            ComputerWinner = true;
-        alert("ComputerWins this round!");
-    } else if (playerChoice === "Scissors" && computerChoice === "Rock") {
-            ComputerWinner = true;
-        alert("ComputerWins this round!");
-    } else if (playerChoice === "Scissors" && computerChoice === "Scissors") {
-        alert("You're even for now!");
-    } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
-            HumanWinner = true;
-        alert("PlayerWins this round!");
-    } else if (playerChoice === "Paper" && computerChoice === "Rock") {
-            HumanWinner = true;
-        alert("PlayerWins this round!");
-    } else if (playerChoice === "Paper" && computerChoice === "Paper") {
-        alert("You're even for now!");
-    } else if (playerChoice === "Paper" && computerChoice === "Scissors") {
-            ComputerWinner = true;
-        alert("ComputerWins this round!");
+let computerSelection = "";
+let playerSelection = "";
+let HumanScore = 0;
+let ComputerScore = 0; 
+const maxScore = 3;
+
+const btnOne = document.querySelector('.BtnOne');
+btnOne.addEventListener('click', () => {
+    if(HumanScore === 3 || ComputerScore === 3) {
+        invalid();
     } else {
-        alert("Input not recognized. Make sure you enter 'Rock', 'Paper' or 'Scissors'. Try again!");
+    playerSelection = "paper";
+    computerSelection = getComputerChoice();
+    round(playerSelection, computerSelection);
+    checkEnd();
     }
-        //  update the scores
-        if(HumanWinner === true) {
-            playerScore = playerScore + 1;
-            document.getElementById("sH").innerHTML = playerScore;
-        } else if (ComputerWinner === true) {
-            computerScore = computerScore + 1;
-            document.getElementById("sC").innerHTML = computerScore;
-        }
+  });
 
-        if(playerScore === 4) {
-            alert("You've won the game!");
-        } else if (computerScore === 4) {
-            alert("Computer won the game!");
-        } else if(numberOfRounds === 4 && playerScore !== 4 && computerScore !== 4) {
-            alert("Nobody wins the game...");
-        }
-    
-      }
-      
-function game() {
-    for (i = 0; i <= 4; i++) {
-        playRound();
-            numberOfRounds = numberOfRounds + 1;
-        }
+const btnTwo = document.querySelector('.BtnTwo');
+btnTwo.addEventListener('click', () => {
+    if(HumanScore === 3 || ComputerScore === 3) {
+        invalid();
+    } else {
+    playerSelection = "rock";
+    computerSelection = getComputerChoice();
+    round(playerSelection, computerSelection);
+    checkEnd();
     }
+  });
 
-document.getElementById("myBtn").addEventListener("click", game);
+const btnThree = document.querySelector('.BtnThree');
+btnThree.addEventListener('click', () => {
+    if(HumanScore === 3 || ComputerScore === 3) {
+        invalid();
+    } else {
+    playerSelection = "scissors";
+    computerSelection = getComputerChoice();
+    round(playerSelection, computerSelection);
+    checkEnd();
+    }
+  });
+
+const btnFour = document.querySelector('.BtnFour');
+btnFour.addEventListener('click', () => {
+    end();
+});
+
+function round(playerSelection, computerSelection) {
+
+    if(playerSelection === "rock" && computerSelection === "scissors") {
+        HumanScore = HumanScore + 1;
+        document.getElementById("sH").textContent = HumanScore;
+    } else if(playerSelection === "rock" && computerSelection === "rock") {
+        return;
+    } else if(playerSelection === "rock" && computerSelection === "paper") {
+        ComputerScore = ComputerScore + 1;
+        document.getElementById("sC").textContent = ComputerScore;
+    } else if(playerSelection === "paper" && computerSelection === "scissors") {
+        ComputerScore = ComputerScore + 1;
+        document.getElementById("sC").textContent = ComputerScore;
+    } else if(playerSelection === "paper" && computerSelection === "rock") {
+        HumanScore = HumanScore + 1;
+        document.getElementById("sH").textContent = HumanScore;
+    } else if(playerSelection === "paper" && computerSelection === "paper") {
+        return;
+    } else if(playerSelection === "scissors" && computerSelection === "scissors") {
+        return;
+    } else if(playerSelection === "scissors" && computerSelection === "rock") {
+        ComputerScore = ComputerScore + 1;
+        document.getElementById("sC").textContent = ComputerScore;
+    } else if(playerSelection === "scissors" && computerSelection === "paper") {
+        HumanScore = HumanScore + 1;
+        document.getElementById("sH").textContent = HumanScore;
+    };
+};
+
+function end() {
+    HumanScore = 0;
+    document.getElementById("sH").textContent = HumanScore;
+    ComputerScore = 0;
+    document.getElementById("sC").textContent = ComputerScore;
+}
+
+function checkEnd() {
+    if(HumanScore === maxScore) {
+        alert("HUMAN WINS!");
+    } else if(ComputerScore === maxScore) {
+        alert("COMPUTER WINS!");
+    }
+}
+
+function invalid() {
+    alert("Oops! it seems the game has ended...you might want to press the RESET button to play another game!");
+}
